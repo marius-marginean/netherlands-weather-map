f1f66ca06e9c93a1ff3af2997b6dbb492aaa7931
marius-marginean <mmariusandrei05@gmail.com>
2024-11-08 22:30:05 +0100

    Standardised Docs to google format

--- weather_scraper.py
+++ weather_scraper.py
@@ -4,19 +4,20 @@ import folium
 import re
 
 def bbc_weather_scraper(url):
-  '''
-  This function is a web scraper that obtains the current weather data in a specific city from a provided bbc weather url. The expected output is a list containing the city name, maximum temperature and description of current conditions. All values are returned as strings.
+  """
+  This function is a web scraper that obtains the current weather data in a specific city from a provided BBC Weather URL. 
+  Args:
+    url (str): URL of the form 'https://www.bbc.com/weather/xxxxxxx'.
+  Returns:
+      list: A list containing:
+        - City (str): The name of the city.
+        - max_temp (int): Maximum temperature in Celsius.
+        - description (str): Description of current conditions. 
 
-  Input: 
-  url(str) of the form : 'https://www.bbc.com/weather/xxxxxxx'.
-
-  Output: 
-  array of the fomr: [City(str), max_temp (int), description (str)] 
-  
-  Example functionality:
-  input:'https://www.bbc.com/weather/2988507'
-  output: ['Paris', 14, 'Sunny and light winds'] //values may change depending on current conditions
-  '''
+  Example:
+        >>> bbc_weather('https://www.bbc.com/weather/2988507')
+        ['Paris', 14, 'Sunny and light winds']  # Values may vary depending on current conditions.
+  """
   #reading of the url
   # A url was chosen as an input instead of a city name because weather websites do not index their results by city name and this makes them much more difficult to scrape.
   if url[0:28] != 'https://www.bbc.com/weather/':
@@ -43,16 +44,23 @@ def bbc_weather_scraper(url):
   return results
 
 def dutch_coordinates(city):
-  '''
-  Function that uses a webscraper to determine the coordinates of a Dutch city from its name. The function is not case sensitive and can deal with names composed of multiple words. 
+  """
+  Determines the geographic coordinates of a specified Dutch city using a web scraper.
 
-  Input: Name of a city in The Netherlands as a string
-  Output: Float array of coordinates in the decimal degree format : [Latitude,Longitude]
+  The function takes the name of a city in the Netherlands, is case-insensitive, and can handle names with multiple words.
+
+  Args:
+      city (str): The name of a Dutch city.
+
+  Returns:
+      list of float: A list containing the coordinates of the city in decimal degree format:
+        - Latitude (float): The latitude of the city.
+        - Longitude (float): The longitude of the city.
 
   Example:
-  Input: 'The Hague'
-  Output: [52.07667, 4.29861]
-  '''
+      >>> dutch_coordinates('The Hague')
+      [52.07667, 4.29861]
+  """
   if type(city) is not str:
     raise ValueError(f"Invalid input data type")
   city=city.lower() #the url only works for lowercase city names
@@ -70,12 +78,29 @@ def dutch_coordinates(city):
   return processed_cooridnates
 
 def weather_array_stacker(url_list):
-  '''
-  Uses the bbc_weather_scraper() and dutch_coordinates() functions to generate a matrix with cities, maximum temperature, weather conditions and coordinates from an array of bbc weather urls. The urls have to correspond to dutch cities in order for the dutch_coordinates() function to work. 
-  
-  Input: Array of urls of the form ["https://www.bbc.com/weather/xxxxxxx","https://www.bbc.com/weather/yyyyyyy"]
-  Output: [[City1(str), temp_max1(int), weather1(str), latitude1(float), longitute1(float)],[City2(str), temp_max2(int), weather2(str), latitude2(float), longitute2(float)]]
-  '''
+  """
+  Generates a matrix with city names, maximum temperatures, weather conditions, and coordinates using a list of BBC Weather URLs.
+
+  This function uses `bbc_weather_scraper()` to obtain weather data and `dutch_coordinates()` to retrieve geographic coordinates.
+  Note: The URLs must correspond to cities in the Netherlands for the `dutch_coordinates()` function to work correctly.
+
+  Args:
+     url_list (list of str): A list of BBC Weather URLs in the format 
+        ["https://www.bbc.com/weather/xxxxxxx", "https://www.bbc.com/weather/yyyyyyy"], where each URL is specific to a Dutch city.
+
+  Returns:
+     list of list: A matrix where each inner list contains:
+        - City (str): The name of the city.
+        - temp_max (int): The maximum temperature in Celsius.
+        - weather (str): Description of current weather conditions.
+        - latitude (float): The latitude of the city in decimal degree format.
+        - longitude (float): The longitude of the city in decimal degree format.
+
+  Example:
+      >>> weather_array_stacker(["https://www.bbc.com/weather/2988507", "https://www.bbc.com/weather/2759794"])
+      [['Paris', 14, 'Sunny and light winds', 52.07667, 4.29861],
+      ['Amsterdam', 10, 'Partly cloudy', 52.3676, 4.9041]]
+  """
   index=len(url_list)#determining number of rows in the matrix
   stacked_results=[]#initialising stack of results
   if index == 0 or type(url_list) is not list:
@@ -89,16 +114,24 @@ def weather_array_stacker(url_list):
   return stacked_results
 
 def marker_colour(temperature):
-  '''
-  This function decides the colour of a marker on the weather map based on the temperature in degrees Celsius at that specific location. The marker will be blue for temperatures under 10 degrees, green for temperatures between 10 and 20 degrees and red for any temperature larger than that. 
-  
-  Input:temperature(int)
-  Output:marker colour(string)
+  """
+  Determines the color of a marker on the weather map based on the temperature in degrees Celsius.
+
+ The function assigns:
+    - 'blue' for temperatures below 10 degrees,
+    - 'green' for temperatures between 10 and 20 degrees,
+    - 'red' for temperatures above 20 degrees.
+
+  Args:
+    temperature (int): The temperature in degrees Celsius.
+
+  Returns:
+    colour (str): The color of the marker ('blue', 'green', or 'red').
 
   Example:
-  Input: 30
-  Output: 'red'
- '''
+    >>> marker_colour(30)
+    'red'
+  """
   if type(temperature) is not int:
     raise ValueError("Invalid input data type")
   if temperature<10:
@@ -111,14 +144,24 @@ def marker_colour(temperature):
   return colour
 
 def map_generator(url_list):
-  '''
-  Function that uses folium to create a map of The Netherlands where all of the cities are assigned their corresponding temperature and weather descriptions. Displays the temperature in the correct location on the map. Colours number blue for temperature below 10 degrees Celsius, green for  under 20 degrees Celsius, and red for anything more, using the marker_colour() function. Clicking on the pop up provides information on weather conditions. Uses the same input as weather_array_stacker because it uses it to generate the map data.
+  """
+  Creates an interactive weather map of The Netherlands using Folium, where each city is marked with its corresponding temperature and weather description.
 
-  Input: 
-  Array of urls of the form ["https://www.bbc.com/weather/xxxxxxx","https://www.bbc.com/weather yyyyyyy"]
-  Output: 
-  creates an html file named 'netherlands_weather_map.html' that can be opened in a separate web browser
-  '''
+  This function uses `weather_array_stacker()` to gather weather data and `marker_colour()` to determine the color of the marker based on the temperature. The map displays:
+    - A color-coded marker (blue for temperatures below 10°C, green for temperatures between 10-20°C, red for temperatures above 20°C).
+    - A popup with weather conditions that appear when clicking on a marker.
+
+  Args:
+      url_list (list of str): A list of BBC Weather URLs in the form 
+        ["https://www.bbc.com/weather/xxxxxxx", "https://www.bbc.com/weather/yyyyyyy"], where each URL corresponds to a Dutch city.
+
+  Returns:
+      None: The function generates an HTML map and saves it as 'netherlands_weather_map.html' for viewing in a web browser.
+
+  Example:
+      >>> map_generator(["https://www.bbc.com/weather/2988507", "https://www.bbc.com/weather/2759794"])
+      # Creates 'netherlands_weather_map.html' with markers showing weather data for cities in the URLs.
+  """
   matrix = weather_array_stacker (url_list)
   netherlands_map = folium.Map(location=[52.3784, 4.9009], zoom_start=7)#location of the map
   index = len(matrix)#number of markers
